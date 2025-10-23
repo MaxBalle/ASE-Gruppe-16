465f8b1b04be9c6d7f5c9cb8f57d359dc6fd1a54
LeQuan14 <anh14quan.le@gmail.com>
2025-10-23 11:53:52 +0200

    test/init Aufgabe02

---
+++ Blatt01/Aufgabe02/main/BowlingGame.java
@@ -0,0 +1,13 @@
+package Blatt01.Aufgabe02.main;
+
+public class BowlingGame {
+
+    public void roll(int pins) {
+        //TODO
+    }
+
+    public int score() {
+        //TODO
+        return 0;
+    }
+}
