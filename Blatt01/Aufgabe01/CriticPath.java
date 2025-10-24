0cf2b7b5c942389de95a4ac02c756062f1e9fa58
st173100 <st173100@stud.uni-stuttgart.de>
2025-10-24 14:27:44 +0200

    New class and Lists

--- Blatt01/Aufgabe01/CriticPath.java
+++ Blatt01/Aufgabe01/CriticPath.java
@@ -1,5 +1,17 @@
 package Blatt01.Aufgabe01;
 
-public class CriticPath {
+import java.util.ArrayList;
+import java.util.List;
 
+public class CriticPath {
+    private List<Workpackage> startNodes = new ArrayList<Workpackage>();
+	private List<Workpackage> endNodes = new ArrayList<Workpackage>();
+
+    public List<Workpackage> getStartNodes() {
+		return startNodes;
+	}
+
+	public void setStartNodes(List<Workpackage> startNodes) {
+		this.startNodes = startNodes;
+	}
 }
---
+++ Blatt01/Aufgabe01/Workpackage.java
@@ -0,0 +1,5 @@
+package Blatt01.Aufgabe01;
+
+public class Workpackage {
+
+}
