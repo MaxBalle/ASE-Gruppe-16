9b2501f5ae8dd99a2ff84a2266be6a588dc22f94
MaxBalle <max.balle@web.de>
2025-10-26 22:04:04 +0100

    Tests and Filling frames

--- Blatt01/Aufgabe02/main/Frame.java
+++ Blatt01/Aufgabe02/main/Frame.java
@@ -5,6 +5,7 @@ public class Frame {
     private int frameNumber;
     private Optional<Integer> firstScore;
     private Optional<Integer> secondScore;
+    private Optional<Integer> thirdScore;
     private boolean spare;
     private boolean strike;
 
@@ -13,28 +14,30 @@ public class Frame {
         this.frameNumber = frameNumber;
         this.firstScore = Optional.empty();
         this.secondScore = Optional.empty();
+        this.thirdScore = Optional.empty();
         this.spare = false;
         this.strike = false;
     }
 
     public void addScore(int pins) {
+        if (isComplete()) throw new Error("Frame is already complete!");
 
         if (firstScore.isEmpty()) {
             this.firstScore = Optional.of(pins);
             if (pins == 10) {
                 this.strike = true;
             }
-        }
-        else if (secondScore.isPresent()) {
-            throw new Error("Frame scores already existing!");
-        }
-        else {
+        } else if (secondScore.isEmpty()) {
             this.secondScore = Optional.of(pins);
 
             if (firstScore.get() + secondScore.get() == 10) {
                 this.spare = true;
-            } 
+            }
         }
+    }
+
+    public boolean hasFirstScore() {
+        return firstScore.isPresent();
     }
 
     public int getFirstScore() {
@@ -42,17 +45,29 @@ public class Frame {
         return firstScore.get();
     }
 
+    public boolean hasSecondScore() {
+        return secondScore.isPresent();
+    }
+
     public int getSecondScore() {
 
         return secondScore.get();
     }
 
-    public boolean getSpare() {
+    public boolean hasThirdScore() {
+        return thirdScore.isPresent();
+    }
+
+    public int getThirdScore() {
+        return thirdScore.get();
+    }
+
+    public boolean isSpare() {
 
         return spare;
     }
 
-    public boolean getStrike() {
+    public boolean isStrike() {
 
         return strike;
     }
@@ -67,4 +82,10 @@ public class Frame {
         return frameNumber;
     }
 
+    public boolean isComplete() {
+        if (firstScore.isEmpty()) return false;
+        if (secondScore.isEmpty() && !isStrike()) return false;
+        if (getFrameNumber() == 10 && (isStrike() || isSpare())  && thirdScore.isEmpty()) return false;
+        return true;
+    }
 }
--- Blatt01/Aufgabe02/test/BowlingGameTest.java
+++ Blatt01/Aufgabe02/test/BowlingGameTest.java
@@ -1,10 +1,9 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import BowlingGame;
 public class BowlingGameTest {
 
     @Test
-    void testStandardGame() {
+    public void testStandardGame() {
         BowlingGame game = new BowlingGame();
         game.roll(5);
         game.roll(3);
@@ -12,7 +11,7 @@ public class BowlingGameTest {
     }
 
     @Test 
-    void testSpare() {
+    public void testSpare() {
         BowlingGame game = new BowlingGame();
         game.roll(5);
         game.roll(5);
@@ -23,10 +22,49 @@ public class BowlingGameTest {
     }
 
     @Test 
-    void testStrike() {
+    public void testStrike() {
         BowlingGame game = new BowlingGame(); 
         game.roll(10);
-        game.roll(5)
-        assertEqual(20, game.score8))
+        game.roll(5);
+        assertEquals(20, game.score());
+    }
+
+    @Test
+    public void testSpareEnding() {
+        BowlingGame game = new BowlingGame();
+        for (int i = 1; i <= 9; i++) {
+            game.roll(0);
+            game.roll(0);
+        }
+        assertEquals(0, game.score());
+        game.roll(10);
+        game.roll(1);
+        game.roll(1);
+        assertEquals(12, game.score());
+    }
+
+    @Test
+    public void testStrikeEnding() {
+        BowlingGame game = new BowlingGame();
+        for (int i = 1; i <= 9; i++) {
+            game.roll(0);
+            game.roll(0);
+        }
+        assertEquals(0, game.score());
+        game.roll(5);
+        game.roll(5);
+        game.roll(1);
+        assertEquals(11, game.score());
+    }
+
+    @Test
+    public void test11FrameGame() {
+        BowlingGame game = new BowlingGame();
+        assertThrows(IllegalStateException.class, () -> {
+            for (int i = 1; i <= 11; i++) {
+                game.roll(0);
+                game.roll(0);
+            }
+        });
     }
 }
