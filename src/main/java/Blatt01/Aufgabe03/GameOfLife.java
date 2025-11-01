35f538d6c096011f2941bd0767704fc13a426a32
Luca <st178216@stud.uni-stuttgart.de>
2025-11-01 20:39:30 +0100

    Converted GameOfLife from static methods and fields to instance-based design

--- src/main/java/Blatt01/Aufgabe03/GameOfLife.java
+++ src/main/java/Blatt01/Aufgabe03/GameOfLife.java
@@ -1,6 +1,6 @@
 package Blatt01.Aufgabe03;
 
-import java.awt.*;
+import java.awt.Point;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,55 +10,72 @@ import java.util.List;
 
 public class GameOfLife {
 
-    private static int generation;
+    private int generation;
+    private boolean[][] grid;
+    private final int length;
+    private final int height;
 
     /**
-     * Creates a grid of given size, initializing all cells as dead.
+     * Constructor to initialize the Game of Life grid with given dimensions.
+     *
+     * @param length The length of the grid
+     * @param height The height of the grid
+     */
+    public GameOfLife(int length, int height) {
+        this.generation = 1;
+        this.grid = createGridWithDeadCells(length, height);
+        this.length = length;
+        this.height = height;
+    }
+
+    /**
+     * Creates a grid of given size, initializing all cells as dead. This method is only used in the constructor and
+     * therefore private.
      *
      * @param length The length of the grid
      * @param height The height of the grid
      * @return The created grid
      * @throws NegativeArraySizeException if length or height is negative
      * @throws IllegalArgumentException   if length or height is zero
+     *
+     * ## AI Generated Documentation (AutoComplete, GitHub Copilot) ##
      */
-    public static boolean[][] createGrid(final int length, final int height) {
-        generation = 1;
-        boolean[][] grid;
+    private boolean[][] createGridWithDeadCells(final int length, final int height) {
+
+        boolean[][] newGrid;
 
         if (length == 0 || height == 0) {
             throw new IllegalArgumentException("Error: Grid dimensions must be greater than zero.");
         }
 
         try {
-            grid = new boolean[length][height];
+            newGrid = new boolean[length][height];
         } catch (NegativeArraySizeException e) {
-            System.err.println("Error: Grid dimensions must be non-negative.");
-            throw e;
+            throw new NegativeArraySizeException("Error: Grid dimensions must be non-negative.");
         }
         for (int x = 0; x < length; x++) {
             for (int y = 0; y < height; y++) {
-                grid[x][y] = false;
+                newGrid[x][y] = false;
             }
         }
-        return grid;
+        return newGrid;
     }
+
 
     /**
      * Prints the current state of the grid to the console.
-     *
-     * @param grid The grid to print
      */
-    public static void printGrid(final boolean[][] grid) {
+    public void printGrid() {
         System.out.println();
         System.out.println("Generation: " + generation);
-        System.out.println("X = ALIVE | █ = DEAD");
+        System.out.println("A = ALIVE | D = DEAD");
         System.out.println();
         for (boolean[] cell : grid) {
             for (boolean b : cell) {
                 if (b) {
-                    System.out.print("X ");
+                    System.out.print("A ");
                 } else {
-                    System.out.print("█ ");
+                    System.out.print("D ");
                 }
             }
             System.out.println();
@@ -68,12 +85,11 @@ public class GameOfLife {
     /**
      * Sets the cell at the given point to alive.
      *
-     * @param point The point of the cell to set alive
-     * @param grid  The grid containing the cell
+     * @param point The coordinates of the cell to set alive
      * @throws IndexOutOfBoundsException if the point is out of grid bounds
      */
-    public static void setAlive(final Point point, final boolean[][] grid) {
-        if(!isPartOfGrid(grid, point)) {
+    public void setAlive(final Point point) {
+        if(!isPartOfGrid(point)) {
             throw new IndexOutOfBoundsException("Point " + point + " is out of grid bounds.");
         }
         grid[point.x][point.y] = true;
@@ -82,12 +98,11 @@ public class GameOfLife {
     /**
      * Sets the cell at the given point to dead.
      *
-     * @param point The point of the cell to set dead
-     * @param grid  The grid containing the cell
+     * @param point The coordinates of the cell to set dead
      * @throws IndexOutOfBoundsException if the point is out of grid bounds
      */
-    public static void setDead(final Point point, final boolean[][] grid) {
-        if(!isPartOfGrid(grid, point)) {
+    public void setDead(final Point point) {
+        if(!isPartOfGrid(point)) {
             throw new IndexOutOfBoundsException("Point " + point + " is out of grid bounds.");
         }
         grid[point.x][point.y] = false;
@@ -97,11 +112,10 @@ public class GameOfLife {
      * Checks if the cell at the given point is alive.
      *
      * @param point The point of the cell to check
-     * @param grid  The grid containing the cell
      * @return True if the cell is alive, false otherwise
      */
-    public static boolean isAlive(final Point point, final boolean[][] grid) {
-        boolean partOfGrid = isPartOfGrid(grid, point);
+    public boolean isAlive(final Point point) {
+        boolean partOfGrid = isPartOfGrid(point);
         if (!partOfGrid) {
             return false;
         } else {
@@ -111,31 +125,26 @@ public class GameOfLife {
 
     /**
      * Advances the grid to the next generation based on the rules of Conway's Game of Life.
-     *
-     * @param grid The current grid
-     * @return The grid for the next generation
-     */
-    public static boolean[][] step(final boolean[][] grid) {
+     **/
+    public void step() {
         generation++;
-        int length = grid.length;
-        int height = grid[0].length;
+        int length = this.length;
+        int height = this.height;
         boolean[][] newGrid = new boolean[length][height];
 
+        // Copy current grid to new grid
         for (int x = 0; x < length; x++) {
             System.arraycopy(grid[x], 0, newGrid[x], 0, height);
         }
+
         for (int x = 0; x < length; x++) {
             for (int y = 0; y < height; y++) {
                 Point currentCell = new Point(x, y);
                 List<Point> currentNeighbours = getNeighbours(currentCell);
-                if (isAliveInNextGeneration(grid, currentCell, currentNeighbours)) {
-                    setAlive(currentCell, newGrid);
-                } else {
-                    setDead(currentCell, newGrid);
-                }
+                newGrid[currentCell.x][currentCell.y] = isAliveInNextGeneration(currentCell, currentNeighbours);
             }
         }
-        return newGrid;
+        grid = newGrid;
     }
 
     /**
@@ -144,7 +153,7 @@ public class GameOfLife {
      * @param point The point of the cell
      * @return A list of points representing the neighbouring cells
      */
-    public static List<Point> getNeighbours(final Point point) {
+    public List<Point> getNeighbours(final Point point) {
         List<Point> neighbours = new ArrayList<>();
         int x = point.x;
         int y = point.y;
@@ -166,11 +175,10 @@ public class GameOfLife {
     /**
      * Checks if a point is within the bounds of the grid.
      *
-     * @param grid  The grid
      * @param point The point to check
      * @return True if the point is within the grid, false otherwise
      */
-    public static boolean isPartOfGrid(final boolean[][] grid, final Point point) {
+    public boolean isPartOfGrid(final Point point) {
         return !(point.x < 0 || point.x > grid.length - 1 || point.y > grid[0].length - 1 || point.y < 0);
     }
 
@@ -182,14 +190,15 @@ public class GameOfLife {
      * 3. Stability: Any live cell with two or three live neighbours lives on to the next generation.
      * 4. Birth: Any dead cell with exactly three live neighbours becomes a live cell.
      *
-     * @param grid       The current grid
      * @param cell       The cell to evaluate
      * @param neighbours The list of neighbouring cells
      * @return True if the cell will be alive in the next generation, false otherwise
+     *
+     * ## AI Generated Documentation (AutoComplete, GitHub Copilot) ##
      */
-    public static boolean isAliveInNextGeneration(boolean[][] grid, Point cell, List<Point> neighbours) {
-        int neighboursAlive = getNumberNeighboursAlive(grid, neighbours);
-        if (GameOfLife.isAlive(cell, grid)) {
+    public boolean isAliveInNextGeneration(Point cell, List<Point> neighbours) {
+        int neighboursAlive = getNumberNeighboursAlive(neighbours);
+        if (isAlive(cell)) {
             return neighboursAlive == 2 || neighboursAlive == 3;
         } else {
             return neighboursAlive == 3;
@@ -199,26 +208,24 @@ public class GameOfLife {
     /**
      * Counts the number of alive neighbours for a given cell.
      *
-     * @param grid       The current grid
      * @param neighbours The list of neighbouring cells
      * @return The number of alive neighbours
      */
-    public static int getNumberNeighboursAlive(boolean[][] grid, List<Point> neighbours) {
+    public int getNumberNeighboursAlive(List<Point> neighbours) {
         int neighboursAlive = 0;
         for (Point neighbour : neighbours) {
-            if (GameOfLife.isAlive(neighbour, grid)) {
+            if (isAlive(neighbour)) {
                 neighboursAlive++;
             }
         }
         return neighboursAlive;
     }
 
-    /**
-     * Gets the current generation of the grid.
-     *
-     * @return The current generation
-     */
-    public static int getGeneration() {
-        return generation;
-    }
+    public int getGeneration() {return generation;}
+
+    public int getLength() {return length;}
+
+    public int getHeight() {return height;}
+
+    public boolean[][] getGrid() {return grid;}
 }
